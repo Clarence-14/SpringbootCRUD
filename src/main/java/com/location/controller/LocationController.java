fe24a8b791b20dc8f15ac314fb33f17db067f8db
Clarence-14 <thisisclarence1@gmail.com>
2025-05-15 11:41:14 +0300

    commit

--- src/main/java/com/location/controller/LocationController.java
+++ src/main/java/com/location/controller/LocationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.location.entity.Location;
@@ -18,6 +19,7 @@ import com.location.service.LocationService;
 
 
 @RestController
+@RequestMapping("/locations")
 public class LocationController {
 
     @Autowired
